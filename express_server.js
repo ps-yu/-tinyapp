22c14414e198ce7abf87c2682ab659a3624c2695
ps-yu <pankajsharma12540@gmail.com>
2022-12-01 21:15:23 -0500

    Use a post method to get the short url from the user

--- express_server.js
+++ express_server.js
@@ -2,7 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const generateRandomString = () => {
+  var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  var result = ""
+  var charactersLength = characters.length;
+
+  for ( var i = 0; i < 5 ; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
 app.set("view engine", "ejs");
+
+//To parse the data used by the post in human readable form
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,11 +39,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   console.log(req.params)
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 
