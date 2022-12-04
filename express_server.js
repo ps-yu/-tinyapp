6d67424004c4c72769a7c5e2e7b075f6c016cc3c
ps-yu <pankajsharma12540@gmail.com>
2022-12-04 15:17:33 -0500

    Create login/logout button

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const { redirect } = require("statuses");
 const app = express();
+app.use(cookieParser());
 const PORT = 8080; // default port 8080
 
 const generateRandomString = () => {
@@ -14,6 +16,7 @@ const generateRandomString = () => {
 
   return result;
 }
+
 
 app.set("view engine", "ejs");
 
@@ -34,39 +37,64 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//To read the url in the database
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username : req.cookies["username"],
+    urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//To vist the url webpage
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id]
   res.redirect(longURL);
 });
 
+//To create a new url
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//To dynamically show the requested short url
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { 
+    username : req.cookies["username"],
+    id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+//To create cookie when the user log's in
+app.post("/login", (req,res) => {
+  res.cookie("username", req.body.login);
+  res.redirect("/urls");
+});
+
+//To logout the user
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls")
+})
+
+//To crearte short url for the requested website
 app.post("/urls", (req, res) => {
   const randomString = generateRandomString();
   urlDatabase[randomString] = req.body.longURL;
+  res.redirect("/urls")
 });
 
+//To delete an existing url
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls")
 })
 
+//To update an existing url
 app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.updatedURL;
   res.redirect("/urls")
 })
+
 
 
 app.listen(PORT, () => {
