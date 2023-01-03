1e924a20b824a6c9d7b721f833b381b3b9e2503f
ps-yu <pankajsharma12540@gmail.com>
2023-01-02 21:03:36 -0500

    Add database file

---
+++ database.js
@@ -0,0 +1,21 @@
+//////////////
+//Databases//
+/////////////
+
+//Database to store the urls
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+//Database to store the userID and passwords
+const users = {
+};
+
+module.exports = {urlDatabase,users};
